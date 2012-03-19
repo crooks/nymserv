45510971edbc248a976a5d3610543cd4435137b6
Steve Crook <steve@mixmin.net>
2012-03-19 13:53:06 +0000

    Make the gpgparse an object and tweak nymserv to use it as such.

--- nymserv/gnupg.py
+++ nymserv/gnupg.py
@@ -38,7 +38,6 @@ class GnupgError(Error):
         return repr(self.expr)
 
 
-# Superclass GnuPGInterface.GnuPG
 class GnuPGFunctions():
     def __init__(self, keyring=None):
         self.gnupg = GnuPGInterface.GnuPG()
@@ -104,9 +103,11 @@ class GnuPGFunctions():
         self.gnupg.options.extra_args = ['--with-fingerprint']
         idlist = [keyid]
         proc = self.gnupg.run(['--list-keys'], args=idlist,
-                                               create_fhs=['stdout'])
+                                               create_fhs=['stdout',
+                                                           'logger'])
         result = proc.handles['stdout'].read()
         proc.handles['stdout'].close()
+        proc.handles['logger'].close()
         return result
 
     def fingerprint(self, keyid):
@@ -116,9 +117,11 @@ class GnuPGFunctions():
         self.reset_options()
         idlist = [keyid]
         proc = self.gnupg.run(['--fingerprint'], args=idlist,
-                                                 create_fhs=['stdout'])
+                                                 create_fhs=['stdout',
+                                                             'logger'])
         result = proc.handles['stdout'].read()
         proc.handles['stdout'].close()
+        proc.handles['logger'].close()
         lines = result.split("\n")
         fps = []
         lastline = ""
@@ -151,6 +154,25 @@ class GnuPGFunctions():
                 if uidmail_match:
                     uidmail.append(uidmail_match.group(0))
         return uidmail
+
+    def listkeys(self):
+        """Return a list of all the keys on the keyring.
+        
+        """
+        self.reset_options()
+        proc = self.gnupg.run(['--list-keys'], create_fhs=['stdout'])
+        result = proc.handles['stdout'].read()
+        proc.handles['stdout'].close()
+        proc.wait()
+        keys = []
+        # UID lines
+        lines = result.split("\n")
+        for line in lines:
+            if line.startswith("pub "):
+                foo, ktmp = line.split("/", 1)
+                k, foo = ktmp.split(" ", 1)
+                keys.append(k)
+        return keys
 
     def delete_key(self, keyid):
         """Delete a public key. Note: This doesn't return a status. It either
@@ -276,14 +298,40 @@ class GnuPGStatParse():
 
     def __init__(self):
         """Define all the regular expressions required to populate the GnuPG
-        Status dictionary."""
+        Status dictionary.
+
+        """
+        key = '[0-9A-F]{8}'
+        date = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+        keytype = '[0-9]{3,6}[RDg]'
+
         # gpg: encrypted with 2048-bit ELG-E key, ID F207AEDB,
         # created 2003-06-04
-        enc = "gpg: encrypted with (\d+)-bit"
-        enc += " (\S+) key"
-        enc += ".*ID ([0-9A-F]+)"
-        enc += ".*created ([0-9\-]+)$"
+        enc = 'gpg: encrypted with (\d+)-bit'
+        enc += ' (\S+) key'
+        enc += '.*ID (%s)' % key
+        enc += ".*created (%s)$" % date
         enc_re = re.compile(enc)
+
+        # pub   1024D/228761E7 2003-06-04
+        gpg_pub = 'pub +(%s)/(%s) (%s)' % (keytype, key, date)
+        self.gpg_pub = re.compile(gpg_pub)
+        # pub   4096R/1D6A8052 2010-12-13 [expired: 2011-12-13]
+        gpg_pub_expired = '%s \[expired: (%s)\]' % (gpg_pub, date)
+        self.gpg_pub_expired = re.compile(gpg_pub_expired)
+        # pub   4096R/1B0AAE44 2010-12-28 [expires: 2015-12-27]
+        gpg_pub_expires = '%s \[expires: (%s)\]' % (gpg_pub, date)
+        self.gpg_pub_expires = re.compile(gpg_pub_expires)
+
+        # sub   1024D/228761E7 2003-06-04
+        gpg_sub = 'sub +(%s)/(%s) (%s)' % (keytype, key, date)
+        self.gpg_sub = re.compile(gpg_sub)
+        # sub   4096R/1D6A8052 2010-12-13 [expired: 2011-12-13]
+        gpg_sub_expired = '%s \[expired: (%s)\]' % (gpg_sub, date)
+        self.gpg_sub_expired = re.compile(gpg_sub_expired)
+        # sub   4096R/1B0AAE44 2010-12-28 [expires: 2015-12-27]
+        gpg_sub_expires = '%s \[expires: (%s)\]' % (gpg_sub, date)
+        self.gpg_sub_expires = re.compile(gpg_sub_expires)
 
         # gpg: decryption failed: secret key not available
         decfail_re = re.compile("gpg: decryption failed: (.*)")
@@ -301,12 +349,6 @@ class GnuPGStatParse():
         # 228761E7
         sigmade = "gpg: Signature made (.*) using (.*) key ID ([0-9A-F]+)"
         sigmade_re = re.compile(sigmade)
-
-        # pub   1024D/228761E7 2003-06-04
-        pub_re = re.compile("pub +([^/]+)/([0-9A-F]+) ([0-9\-]+)")
-
-        # sub   1024D/228761E7 2003-06-04
-        sub_re = re.compile("sub +([^/]+)/([0-9A-F]+) ([0-9\-]+)")
 
         # Key fingerprint = 1CD9 95E1 E9CE 80D6 C885  B7EB B471 80D5 2287 61E7
         # Primary key fingerprint: 0123 4567 89AB CDEF
@@ -345,45 +387,60 @@ class GnuPGStatParse():
         self.import_re = import_re
         self.import_nc_re = import_nc_re
         self.import_novalid_re = re.compile(imp)
-        self.pub_re = pub_re
-        self.sub_re = sub_re
         self.fingerprint_re = fingerprint_re
         self.verifyfail = verifyfail
 
-    def __strip_gpg(self, text):
+    def __getitem__(self, key):
+        return self.gpgstat[key]
+
+    def __iter__(self):
+        return iter(self.gpgstat)
+
+    def _strip_gpg(self, text):
         gpg, rest = text.split(": ", 1)
         return rest
 
-    def __epoch_from_datestr(self, datestr):
+    def _epoch_from_datestr(self, datestr):
         """Return epoch seconds when passed a date string formatted as:
-        2011-07-11.  GnuPG seems to use this format for key dates."""
-        pattern = '%Y-%m-%d'
-        return int(time.mktime(time.strptime(datestr, pattern)))
+        2011-07-11.  GnuPG seems to use this format for key dates.
+
+        """
+        return int(time.mktime(time.strptime(datestr, '%Y-%m-%d')))
+
+    def _listdict_append(self, key, value):
+        if key in self.gpgstat:
+            self.gpgstat[key].append(value)
+        else:
+            self.gpgstat[key] = [value]
 
     def statparse(self, status):
         """Take a GnuPG Status message and parse it into specific dictionary
         elements. We might not capture every possible element but at least all
-        those we need to perform effective validation."""
+        those we need to perform effective validation.
+
+        """
+        # Dictionary of GPG status that we'll return
+        self.gpgstat = {}
+
         # All status lines begin with gpg: but we'll let the regexs take care
         # of that for us.
         lines = status.split("\n")
         lastline = None
-        gpgstat = {}  # Dictionary of GPG status that we'll return
         for line in lines:
             # First we'll look for errors as there's no point populating a
             # dict that's not returned.
             # gpg: CRC error; 44FB85 - C2B77E
             if line.startswith("gpg: CRC error"):
-                err = self.__strip_gpg(line)
+                err = self._strip_gpg(line)
                 raise GnupgError(err)
 
             enc_match = self.enc_re.match(line)
             if enc_match:
-                gpgstat['ekeylen'] = enc_match.group(1)
-                gpgstat['ekeytype'] = enc_match.group(2)
-                gpgstat['ekeyid'] = enc_match.group(3)
+                self.gpgstat['ekeylen'] = enc_match.group(1)
+                self.gpgstat['ekeytype'] = enc_match.group(2)
+                self.gpgstat['ekeyid'] = enc_match.group(3)
                 encdate = enc_match.group(4)
-                gpgstat['ekeydate'] = self.__epoch_from_datestr(encdate)
+                self.gpgstat['ekeydate'] = self._epoch_from_datestr(encdate)
 
             sigmade_match = self.sigmade_re.match(line)
             if sigmade_match:
@@ -391,133 +448,160 @@ class GnuPGStatParse():
                 # epoch format.
                 sigdate = sigmade_match.group(1)
                 sigepoch = email.utils.parsedate(sigdate)
-                gpgstat['sigdate'] = time.mktime(sigepoch)
-                gpgstat['sigtype'] = sigmade_match.group(2)
-                gpgstat['keyid'] = sigmade_match.group(3)
+                self.gpgstat['sigdate'] = time.mktime(sigepoch)
+                self.gpgstat['sigtype'] = sigmade_match.group(2)
+                self.gpgstat['keyid'] = sigmade_match.group(3)
 
             fingerprint_match = self.fingerprint_re.search(line)
             # Check for lastline prevents picking up the fingerprint for subkey
             # instead of the primary.
             if fingerprint_match and not lastline.startswith("sub"):
                 fp = fingerprint_match.group(1)
-                gpgstat['fingerprint'] = fp.replace(" ", "")
+                self.gpgstat['fingerprint'] = fp.replace(" ", "")
 
             decfail_match = self.decfail_re.match(line)
             if decfail_match:
-                gpgstat['decfail'] = True
-                gpgstat['decfailreason'] = decfail_match.group(1)
+                self.gpgstat['decfail'] = True
+                self.gpgstat['decfailreason'] = decfail_match.group(1)
 
             pkdfail_match = self.pkdfail_re.match(line)
             if pkdfail_match:
-                gpgstat['pkdfail'] = True
-                gpgstat['pkdfailreason'] = pkdfail_match.group(1)
+                self.gpgstat['pkdfail'] = True
+                self.gpgstat['pkdfailreason'] = pkdfail_match.group(1)
 
             goodsig_match = self.goodsig_re.match(line)
             if goodsig_match:
-                gpgstat['goodsig'] = True
+                self.gpgstat['goodsig'] = True
                 txt = goodsig_match.group(1)
-                if 'uidtext' not in gpgstat:
-                    gpgstat['uidtext'] = []
-                gpgstat['uidtext'].append(txt)
+                if 'uidtext' not in self.gpgstat:
+                    self.gpgstat['uidtext'] = []
+                self.gpgstat['uidtext'].append(txt)
 
                 # Perform a sub-match on the sig text to see if it contains a
                 # valid email address.
                 goodsigmail_match = self.email_re.search(txt)
                 if goodsigmail_match:
-                    if 'uidmail' not in gpgstat:
-                        gpgstat['uidmail'] = []
-                    gpgstat['uidmail'].append(goodsigmail_match.group(0))
+                    if 'uidmail' not in self.gpgstat:
+                        self.gpgstat['uidmail'] = []
+                    self.gpgstat['uidmail'].append(goodsigmail_match.group(0))
 
             # AKA lines in key signatures
             akasig_match = self.akasig_re.match(line)
             if akasig_match:
                 txt = akasig_match.group(1)
-                if 'uidtext' not in gpgstat:
+                if 'uidtext' not in self.gpgstat:
                     # This should never happen.  Can't get an aka without first
                     # having processed a signature. Processing that signature
                     # would create the 'uidtext' element.
                     raise GnupgError("Unexpected aka before signature")
-                gpgstat['uidtext'].append(txt)
+                self.gpgstat['uidtext'].append(txt)
                 # Perform a sub-match on the sig text to see if it contains a
                 # valid email address.
                 akasigmail_match = self.email_re.search(txt)
                 if akasigmail_match:
-                    if 'uidmail' not in gpgstat:
+                    if 'uidmail' not in self.gpgstat:
                         # This is a valid condition because the Signature line
                         # might contain a non-email uid.
-                        gpgstat['uidmail'] = []
-                    gpgstat['uidmail'].append(akasigmail_match.group(0))
+                        self.gpgstat['uidmail'] = []
+                    self.gpgstat['uidmail'].append(akasigmail_match.group(0))
 
             # UID lines
             if line.startswith("uid"):
                 foo, uid = line.split(" ", 1)
                 uid = uid.strip()
-                if 'uidtext' not in gpgstat:
-                    gpgstat['uidtext'] = []
-                gpgstat['uidtext'].append(uid)
+                if 'uidtext' not in self.gpgstat:
+                    self.gpgstat['uidtext'] = []
+                self.gpgstat['uidtext'].append(uid)
                 uidmail_match = self.email_re.search(uid)
                 if uidmail_match:
-                    if 'uidmail' not in gpgstat:
-                        gpgstat['uidmail'] = []
-                    gpgstat['uidmail'].append(uidmail_match.group(0))
+                    if 'uidmail' not in self.gpgstat:
+                        self.gpgstat['uidmail'] = []
+                    self.gpgstat['uidmail'].append(uidmail_match.group(0))
 
             # PUB lines
-            pub_match = self.pub_re.match(line)
+            pub_match = self.gpg_pub.match(line)
             if pub_match:
-                gpgstat['sigtype'] = pub_match.group(1)
-                gpgstat['keyid'] = pub_match.group(2)
+                pub_expires_match = self.gpg_pub_expires.match(line)
+                pub_expired_match = self.gpg_pub_expired.match(line)
+                if pub_expires_match:
+                    expires = pub_expires_match.group(4)
+                    self.gpgstat['expires'] = self._epoch_from_datestr(expires)
+                elif pub_expired_match:
+                    expired = pub_expired_match.group(4)
+                    self.gpgstat['expired'] = self._epoch_from_datestr(expired)
+                self.gpgstat['sigtype'] = pub_match.group(1)
+                self.gpgstat['keyid'] = pub_match.group(2)
                 sigdate = pub_match.group(3)
-                gpgstat['sigdate'] = self.__epoch_from_datestr(sigdate)
+                self.gpgstat['sigdate'] = self._epoch_from_datestr(sigdate)
 
             # SUB lines
-            sub_match = self.sub_re.match(line)
+            sub_match = self.gpg_sub.match(line)
             if sub_match:
-                gpgstat['subtype'] = sub_match.group(1)
-                gpgstat['subkeyid'] = sub_match.group(2)
+                sub_expires_match = self.gpg_sub_expires.match(line)
+                sub_expired_match = self.gpg_sub_expired.match(line)
+                if sub_expires_match:
+                    expires = sub_expires_match.group(4)
+                    self._listdict_append('sub_expires',
+                                          self._epoch_from_datestr(expires))
+                else:
+                    # When no match occurs, we need to append something, else
+                    # lists of subkeys aren't synced with eachother.
+                    self._listdict_append('sub_expires', None)
+
+                if sub_expired_match:
+                    expired = sub_expired_match.group(4)
+                    self._listdict_append('sub_expired',
+                                          self._epoch_from_datestr(expired))
+                else:
+                    self._listdict_append('sub_expired', None)
+
+                self._listdict_append('sub_sigtype', sub_match.group(1))
+                self._listdict_append('sub_keyid', sub_match.group(2))
                 sigdate = sub_match.group(3)
-                gpgstat['subdate'] = self.__epoch_from_datestr(sigdate)
+                self._listdict_append('sub_sigdate',
+                                      self._epoch_from_datestr(sigdate))
 
             if line == self.nottrusted:
-                gpgstat['trusted'] = False
+                self.gpgstat['trusted'] = False
 
             if line.startswith(self.imported):
                 foo, numpro = line.split("processed: ")
-                gpgstat['imported'] = int(numpro)
+                self.gpgstat['imported'] = int(numpro)
 
             # Importing keys
             import_match = self.import_re.match(line)
             import_nc_match = self.import_nc_re.match(line)
             if import_match:
-                gpgstat['keyid'] = import_match.group(1)
+                self.gpgstat['keyid'] = import_match.group(1)
                 uid = import_match.group(2)
             if import_nc_match:
-                gpgstat['keyid'] = import_nc_match.group(1)
+                self.gpgstat['keyid'] = import_nc_match.group(1)
                 uid = import_nc_match.group(2)
             if import_match or import_nc_match:
-                if 'uidtext' not in gpgstat:
-                    gpgstat['uidtext'] = []
-                gpgstat['uidtext'].append(uid)
+                if 'uidtext' not in self.gpgstat:
+                    self.gpgstat['uidtext'] = []
+                self.gpgstat['uidtext'].append(uid)
                 email_match = self.email_re.search(uid)
                 if email_match:
-                    if 'uidmail' not in gpgstat:
-                        gpgstat['uidmail'] = []
-                    gpgstat['uidmail'].append(email_match.group(0))
+                    if 'uidmail' not in self.gpgstat:
+                        self.gpgstat['uidmail'] = []
+                    self.gpgstat['uidmail'].append(email_match.group(0))
 
             # Verify Failure
             if line.startswith(self.verifyfail):
-                gpgstat['goodsig'] = False
+                self.gpgstat['goodsig'] = False
             # In some instances, (like fingerprint) we need to check the
             # previous line to get the correct match.
             lastline = line
-        return gpgstat
 
 
 def main():
-    g = GnuPGFunctions("/crypt/var/nymserv/keyring")
+    g = GnuPGFunctions()
     gp = GnuPGStatParse()
-    txt = "The cat sat on the matress"
-    print g.import_key(txt)
-    g.reset_options()
+    for k in g.listkeys():
+        gp.statparse(g.keyinfo(k))
+        if 'expired' in gp:
+            print "%(keyid)s" % gp
 
 # Call main function.
 if (__name__ == "__main__"):
